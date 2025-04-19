9b35ac9c293dcbfe037f0fa9686e9250c9a41119
waming <14264805+waming678@user.noreply.gitee.com>
2025-04-19 17:38:54 +0800

    OPENMV-pyDrive

--- OpenMV/main2.py
+++ OpenMV/main2.py
@@ -1,3 +1,8 @@
+#-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+# Untitled - By: lenovo - Fri Apr 11 2025
+
 import sensor, image, time,pyb,json
 from machine import UART
 import time
@@ -5,37 +10,54 @@ sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
+
 clock = time.clock()
+
+# 初始化 UART3，波特率 19200
 uart = pyb.UART(3, 115200)
+
+temp=0
 state=[]
-
-
-
-def uart_scan():
-	if uart.any():
-		uart_rec=uart.readline().decode().strip()
-		return uart_rec
-	else :
-		return 0
-
-def usart_test():
-	for i in range(16):
-		uart.write(i)
-		if(uart_scan()==2):
-			return
-	return
-
 def Receive_Data(data):
-	if data == "1":
-		state.append("a")
-	elif data == "2":
-		state.append("b")
-	elif data == "3":
-		state.append("c")
-	uart.write(str(state) + '\n')
+
+    if data == 1:
+        temp=temp+1
+        uart.write(str(temp))
+    elif data == 2:
+        temp=9
+        uart.write(str(temp))
+    elif data == 3:
+        pass
+        uart.write(str(temp))
+
+    # 转字符串并发送当前状态
+    #注意是！！！字符串！！！damn
+
+
+
+
+
 while True:
-	clock.tick()
-	if uart.any():
-		a=uart.readline().decode().strip()
-		print(a)
-		Receive_Data(a)
+    clock.tick()
+
+    #uart.write(3)  # 发送数据1
+# 等待接收到数据
+    if uart.any():
+
+        data=uart.read(1)
+        print(data)
+        #uart.write(str(data))
+        uart.write(str(data[0]))
+        #data_1=data[0]
+        #a=uart.readline().decode().strip()
+        #strip删掉回车和换行#readline 传送某一个字符；decode 将某一个字符变成字符串
+        #!!!注意是字符串！！！damn
+        #print(data)
+        #接收反馈，用来判断串口无输出的时候，#究竟是1.后半部分函数的问题，2.还是从机就没有接收到？
+        #Receive_Data(data_1)
+
+
+#-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------
+
