93e3cca78c43c7f99daf3b3a1bf0a95f23b92b81
waming <14264805+waming678@user.noreply.gitee.com>
2025-05-02 20:53:31 +0800

    wm_commit2

--- STM32/User/main.c
+++ STM32/User/main.c
@@ -118,23 +118,27 @@ int main(void)
 			 //---------------------------------------------
 			 //侧方停车逻辑
 			 //阶段2-1 继续巡线，直到收到2的信号：1
-			 SERIAL_sendBYTE(USART1,1);
-			 while(!Key_GetNum()){}
-			 SERIAL_sendBYTE(USART3,1);
+			 SERIAL_sendBYTE(USART1,1);		//可视为分段测试，再次给openmv1发送1使其巡线
+			 while(!Key_GetNum()){}			//按下按钮才运行openmv2的模式
+			 SERIAL_sendBYTE(USART3,3);		//模式3为数侧方库边数
+			 
+			 
+			 //此3行代码与上重复，可注释掉
+				 
 			 //舵机回正
 			 drive_setORI(0);
 			 
 			 //阶段2-2，初次入库
 			 SERIAL_sendBYTE(USART3,6);//openmv2的模式需要测距功能，用于为舵机左打提供信号
-			 drive_setDir(3);
+			 drive_setDir(30);		   //初次倒库的角度2，需调节
 			 drive_setORI(-5);
 				 OLED_ShowNum(1,1,9,2);
-			 while(openMV2_mes!=6){}
+			 while(openMV2_mes!=2){}	//直到openmv2识别到方块，执行舵机的第二条指令
 				 OLED_ShowNum(1,1,10,2);
 			 //阶段2-3，舵机左打
 			 drive_setORI(-5);
-			 drive_setDir(3);
-			 Delay_ms(1000);
+			 drive_setDir(-35);         //舵机的测试角度2，需调节
+			 Delay_ms(1000);			//倒车方式2的倒车时间，需测量
 			 drive_setORI(0);//电机停
 			
 			 //阶段2-4
@@ -142,34 +146,39 @@ int main(void)
 			 BUZZER_ON();//undefined
 			 Delay_ms(1000);
 			 BUZZER_OFF();
-				
+			 Delay_ms(4000);			//停车入库，蜂鸣器响1秒，停5秒
+			 
 			 //车头的出库阶段1
 			 //阶段2-5
 				 
-			 SERIAL_sendBYTE(USART3,7);
-			 drive_setDir(0);
-			 drive_setORI(5);
+			 SERIAL_sendBYTE(USART3,2);		//openmv2的模式设置为模式2，用于识别色块
+			 drive_setDir(-35);				//舵机出库角度1，需测试，与入库角度2相同
+			 drive_setORI(5);				
 				 OLED_ShowNum(1,1,11,2);
-			 while(openMV2_mes!=7){};
+			 while(openMV2_mes!=7){};		//直到识别到色块，进行角度2的调节
 				 OLED_ShowNum(1,1,12,2);
-			 drive_setORI(0);
+			 //drive_setORI(0);
 				 
 			 //舵机旋转角度的出库阶段2，先使能openmv1，再延迟数据的接收
 			 //阶段2-6
-			 SERIAL_sendBYTE(USART1,1);
-			 openMV1_mes=0;
-			 drive_setDir(3);
-			 drive_setORI(5);
-			 Delay_ms(1000);
-			 while(!openMV1_mes){}
+			 SERIAL_sendBYTE(USART1,1);     //使能openmv1，使其巡线
+			 openMV1_mes=0;						
+			 drive_setDir(3);				//出库角度2，需测量，与入库角度1相同
+			 drive_setORI(5);				
+			 Delay_ms(1000);				//出库角度为角度2的时候的运行时间，需测量
+			 while(!openMV1_mes){}			//直到识别到线，跳出循环执行pid计算
 			 
 			 //出库车身摆正，继续巡线，并使能openmv2
 			 //阶段7
-			 SERIAL_sendBYTE(USART3,3);
-			 linePatrol(1,100);
+			 SERIAL_sendBYTE(USART3,3);		//模式3为继续计数
+			 linePatrol(1,100);				//继续巡线
 			 
-		
-		
+			 */
+			 
+			 
+			 
+		//可以不管以下代码------------------------------
+		/*
 		//出库阶段1，直行出库
 		SERIAL_sendBYTE(USART3,0xFF);
 		drive_setDir(0);
@@ -193,6 +202,7 @@ int main(void)
 			OLED_ShowNum(1,1,15,2);
 		linePatrol(1,100);
 		*/
+		//---------------------------------------------------------
 	}
 	//openMVTest();
 }
