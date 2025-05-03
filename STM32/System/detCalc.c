59d2a8c447e4c5b4d72c40cf2a508cdf02541125
waming <14264805+waming678@user.noreply.gitee.com>
2025-05-03 16:04:47 +0800

    wamingsubmit

--- STM32/System/detCalc.c
+++ STM32/System/detCalc.c
@@ -7,9 +7,9 @@ void PIDgroupInit(){
 	//角度PID
 	UPPID.circleCalc=0;
 	
-	UPPID.P=1.2;
-	UPPID.I=0.0003;
-	UPPID.D=0.25;
+	UPPID.P=1.3;
+	UPPID.I=0.0005;
+	UPPID.D=4;
 
 	UPPID.IntegralError=0;
 	UPPID.LastError=0;
@@ -32,7 +32,7 @@ void pidCalc(PIDgroup* G,int16_t* output)
 	
   int16_t iError=NextPoint-SetPoint;   	//增量计算
 	if(G->circleCalc)CirCal(&iError);
-	*output=G->P*iError+G->I*IntegralError+G->D*(PrevError-LastError);
+	*output=G->P*iError+G->I*IntegralError+G->D*(iError-LastError);
 	
   G->PrevError=LastError;                                      //存储误差，用于下次计算
   G->LastError=iError;
@@ -51,7 +51,7 @@ void CirCalF(float *Ang){
 void linePatrol(uint8_t endFlag,uint8_t dist){
 	UPPID.target=dist;
 	while(openMV2_mes!=endFlag){
-		Delay_ms(UPSampleRate);
+		Delay_us(UPSampleRate);
 		UPPID.current=openMV1_mes;
 		pidCalc(&UPPID,&UPLF_DIR);
 		OLED_ShowNum(2,1,openMV1_mes,3);
@@ -65,7 +65,7 @@ void linePatrol(uint8_t endFlag,uint8_t dist){
 void linePatrol22(uint8_t dist){
 	UPPID.target=dist;
 	while(!Key_GetNum()){
-		Delay_ms(UPSampleRate);
+		Delay_us(UPSampleRate);
 		UPPID.current=openMV1_mes;
 		pidCalc(&UPPID,&UPLF_DIR);
 		OLED_ShowNum(2,1,openMV1_mes,3);
